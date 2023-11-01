8fe331f5c3f284540fbe0cd3691a9beab42b791b
Eduardo Luiz <eduardoluizzanoto@gmail.com>
2023-11-01 13:23:09 -0300

    Finalização da estrutura dos slides - Slide Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import Head from 'next/head';
+import { ReactNode } from 'react';
 import styles from '../styles/HomeNoAuth.module.scss';
 import HeaderNoAuth from '@/src/components/homeNoAuth/headerNoAuth';
 import PresentationSection from '@/src/components/homeNoAuth/presentationSection';
 import CardSection from '@/src/components/homeNoAuth/cardSection';
+import SlideSection from '@/src/components/homeNoAuth/slideSection';
+import { GetStaticProps } from 'next';
+import courseService, { CourseType } from '@/src/services/courseService';
 
-const HomeNotAuth = function () {
+interface IndexPageProps {
+  children?: ReactNode;
+  course: CourseType[];
+}
+
+const HomeNotAuth = function ({ course }: IndexPageProps) {
   return (
     <>
       <Head>
@@ -22,9 +31,20 @@ const HomeNotAuth = function () {
           <PresentationSection />
         </div>
         <CardSection />
+        <SlideSection newestCourses={course} />
       </main>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await courseService.getNewestCourses();
+  return {
+    props: {
+      course: res.data,
+    },
+    revalidate: 3600 * 24, //Verifica a cada 1 dia
+  };
 };
 
 export default HomeNotAuth;
